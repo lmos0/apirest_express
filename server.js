0b24ad8f22bab529ff33523fe52c54d483de578d
Luís Márcio <luismarcio@MacBook-Air-de-Luis.local>
2023-11-06 17:30:10 -0300

    view e rotas

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const blogRouter = require('./routes/blogRoutes')
+require('dotenv').config()
+
+const port = process.env.PORT || 3500
+
+app.use('/articles', blogRouter) //rotas criadas serão colocadas no final da rota /articles
+app.set('view engine', 'ejs')
+
+app.listen(port, () =>{
+    console.log(`Servidor está rodando na porta ${port}`)
+})
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
